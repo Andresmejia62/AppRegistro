6db95ac2c4ec30c8e92771c0a2823217410f8c22
Andrés Mejía <amejia@uamv.edu.ni>
2026-09-22 22:05:42 -0600

    "correcion javafx.collections"

--- src/main/java/com/appregistros/appregistro/HelloApplication.java
+++ src/main/java/com/appregistros/appregistro/HelloApplication.java
@@ -8,12 +8,19 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("/com/appregistros/appregistro/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 420, 320);
+        stage.setTitle("Sistema de Registro");
         stage.setScene(scene);
+        stage.setMinWidth(420);
+        stage.setMinHeight(320);
         stage.show();
     }
 }
